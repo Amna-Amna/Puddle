e4c112fdd8e84bd16a9d5ad4628d699981579851
Amna-Amna <mailtoamna4@gmail.com>
2023-07-06 02:08:35 +0500

    feat: Integrate user authentication with signup and login pages

--- puddle/urls.py
+++ puddle/urls.py
@@ -1,13 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import index, contact
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('contact/', contact, name='contact'),
-    # Add a space after the comma in the include function
     path('items/', include('item.urls')),
-    path('', index, name='index'),
+    path('', include('core.urls') ),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
