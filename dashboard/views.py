5f05521f89d43f42eb4698705ba4cd872eb2608a
Amna-Amna <mailtoamna4@gmail.com>
2023-07-06 18:53:15 +0500

    feat: Implement a feature that allows users to remove items from the dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from item.models import Item
 
@@ -10,3 +10,4 @@ def index(request):
     return render(request, 'dashboard/index.html',{
         'items':items
     })
+
--- item/urls.py
+++ item/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # Add a slash at the end of the path
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
 ]
