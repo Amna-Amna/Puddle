cebac3c5a7ed79926ac4e9f3b57ac9a243e6da21
Amna-Amna <mailtoamna4@gmail.com>
2023-07-06 16:50:28 +0500

    feat: Enable users to add items to the system

--- item/urls.py
+++ item/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'item'
 urlpatterns = [
     # Add a slash at the end of the path
+    path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
 ]
--- item/views.py
+++ item/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required 
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
-# Create your views here.
+from .forms import NewItemForm
 
+@login_required # add this decorator to protect the detail view
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category = item.category,is_sold = False).exclude(pk=pk)[0:3]
@@ -9,4 +11,24 @@ def detail(request, pk):
     return render(request,'item/detail.html',{
         "item" :item,
         'related_items': related_items
+    })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit = False)
+            item.created_by =  request.user
+            item.save()
+
+            return redirect('item:detail', pk = item.id)
+    else:
+
+        form= NewItemForm()
+
+    return render (request, 'item/form.html',{
+        'form':form,
+        'title':'New item',
     })
