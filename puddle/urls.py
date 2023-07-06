b85957a1a6e8f0c81c31c59eeba50ecf94ce1701
Amna-Amna <mailtoamna4@gmail.com>
2023-07-06 17:53:26 +0500

    feat: Add a dashboard page to show user's items

--- puddle/urls.py
+++ puddle/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('items/', include('item.urls')),
     path('', include('core.urls') ),
+    path("dashboard/", include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
